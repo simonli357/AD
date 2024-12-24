5d6ccd7ed31e8d68937cb55f7233912ba4e4aa48
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-12-23 22:35:40 -0500

    updates

--- src/communication/scripts/connection.py
+++ src/communication/scripts/connection.py
@@ -2,17 +2,16 @@ import threading
 import struct
 from collections import OrderedDict
 from sensor_msgs.msg import Image
-from io import BytesIO
 
 
 class Connection:
     def __init__(self, client_socket):
         self.socket = client_socket
         self.data_actions = OrderedDict({
-            '0x01': self.parse_string,
-            '0x02': self.parse_image
+            b'\x01': self.parse_string,
+            b'\x02': self.parse_image
         })
-        self.types = [key.encode('utf-8') for key in self.data_actions.keys()]
+        self.types = list(self.data_actions.keys())
         self.strings = []
         self.images = []
         threading.Thread(target=self.receive, daemon=True).start()
@@ -38,14 +37,11 @@ class Connection:
                 break
             length = struct.unpack('>I', header[:message_size])[0]
             message_type = header[message_size:header_size]
-            message_type_str = '0x' + message_type.hex()
-            print(length)
-            print(message_type_str)
             # Receive the data based on the length from the header
             data = self.recvall(length)
             # Process the data
-            if message_type_str in self.data_actions:
-                self.data_actions[message_type_str](data)
+            if message_type in self.data_actions:
+                self.data_actions[message_type](data)
 
     def send_string(self, str):
         data = str.encode('utf-8')
@@ -57,7 +53,6 @@ class Connection:
         self.strings.append(data.decode('utf-8'))
 
     def parse_image(self, data):
-        print("parsing")
         try:
             img_msg = Image()
             img_msg.deserialize(data)
