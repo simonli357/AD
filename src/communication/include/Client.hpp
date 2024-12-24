ed6ba26a44ee5e017002e9167671f21b4555d811
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-12-23 22:30:35 -0500

    updates

--- src/communication/include/Client.hpp
+++ src/communication/include/Client.hpp
@@ -3,35 +3,39 @@
 #include <cstdint>
 #include <functional>
 #include <netinet/in.h>
+#include <queue>
 #include <sensor_msgs/Image.h>
 #include <thread>
 #include <vector>
 
 class Client {
-public:
-  // Constructor
-  Client(const char *server_ip, const uint16_t server_port);
-  Client(Client &&) = default;
-  Client(const Client &) = delete;
-  Client &operator=(Client &&) = delete;
-  Client &operator=(const Client &) = delete;
-  ~Client();
-  // Methods
-  void initialize();
-  void send_string(const std::string& str);
+  public:
+	// Constructor
+	Client(const char *server_ip, const uint16_t server_port, const size_t buffer_size);
+	Client(Client &&) = default;
+	Client(const Client &) = delete;
+	Client &operator=(Client &&) = delete;
+	Client &operator=(const Client &) = delete;
+	~Client();
+	// Methods
+	void initialize();
+	std::queue<std::string> &get_strings();
+	void send_string(const std::string &str);
+	void send_image(const sensor_msgs::Image &img);
 
-private:
-  // Fields
-  const char *ip;
-  const uint16_t port;
-  int client_socket;
-  sockaddr_in address;
-  std::thread receive;
-  bool alive = true;
-  std::map<uint8_t, std::function<void(Client*, std::vector<uint8_t>&)>> data_actions;
-  std::vector<uint8_t> data_types;
-  // Methods
-  void listen();
-  void parse_string(std::vector<uint8_t>& data);
-  // std::vector<uint8_t> serialize_image(const sensor_msgs::Image &img);
+  private:
+	// Fields
+	const size_t buffer_size;
+	const size_t header_size = 5;
+	const size_t message_size = 4;
+	bool alive = true;
+	sockaddr_in address;
+	int client_socket;
+	std::thread receive;
+	std::map<uint8_t, std::function<void(Client *, std::vector<uint8_t> &)>> data_actions;
+	std::vector<uint8_t> data_types;
+	std::queue<std::string> strings;
+	// Methods
+	void listen();
+	void parse_string(std::vector<uint8_t> &data);
 };
