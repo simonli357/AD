1b094a74e7255088c7c7965cb4ad2f0507f7c890
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2025-01-02 16:47:27 -0500

    embedded

--- Embedded_Platform/Embedded_2024/source/drivers/steeringmotor.cpp
+++ Embedded_Platform/Embedded_2024/source/drivers/steeringmotor.cpp
@@ -1,35 +1,5 @@
-/**
- * Copyright (c) 2019, Bosch Engineering Center Cluj and BFMC organizers
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
-
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
-
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
-
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
-
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-*/
-
 #include <drivers/steeringmotor.hpp>
-
+#include <periodics/imu.hpp>
 
 namespace drivers{
     /**
@@ -43,11 +13,13 @@ namespace drivers{
     CSteeringMotor::CSteeringMotor(
             PinName f_pwm_pin, 
             float f_inf_limit, 
-            float f_sup_limit
+            float f_sup_limit,
+            periodics::CImu& f_imu
         )
         :m_pwm_pin(f_pwm_pin)
         ,m_inf_limit(f_inf_limit)
         ,m_sup_limit(f_sup_limit)
+        ,m_imu(f_imu)
     {
         // Set the ms_period on the pwm_pin
         m_pwm_pin.period_ms(ms_period); 
@@ -158,5 +130,9 @@ namespace drivers{
      */
     bool CSteeringMotor::inRange(float f_angle){
         return m_inf_limit<=f_angle && f_angle<=m_sup_limit;
+    };
+
+    void CSteeringMotor::setYaw(){
+        yaw = m_imu.getYaw();
     };
 }; // namespace hardware::drivers
