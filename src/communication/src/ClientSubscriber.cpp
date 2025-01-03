471903295ef59da67b2f38a5012f6645fb3c1805
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-03 18:03:04 -0500

    updates

--- src/communication/src/ClientSubscriber.cpp
+++ src/communication/src/ClientSubscriber.cpp
@@ -12,18 +12,18 @@ void ClientSubscriber::init() {
 	depth_sub = node_handle.subscribe("/camera/depth/image_raw", 10, &ClientSubscriber::depth_sub_callback, this);
 	waypoint_sub = node_handle.subscribe("/waypoints", 10, &ClientSubscriber::waypoint_sub_callback, this);
 	sign_sub = node_handle.subscribe("/sign", 10, &ClientSubscriber::sign_sub_callback, this);
-	message_sub = node_handle.subscribe("/lane", 10, &ClientSubscriber::message_sub_callback, this);
+	message_sub = node_handle.subscribe("/message", 10, &ClientSubscriber::message_sub_callback, this);
 	client.initialize();
 }
 
-void ClientSubscriber::road_object_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_float32_multi_array(*data); }
+void ClientSubscriber::road_object_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_road_object(*data); }
 
 void ClientSubscriber::camera_sub_callback(const sensor_msgs::Image::ConstPtr &data) { client.send_image_rgb(*data); }
 
 void ClientSubscriber::depth_sub_callback(const sensor_msgs::Image::ConstPtr &data) { client.send_image_depth(*data); }
 
-void ClientSubscriber::waypoint_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_float32_multi_array(*data); }
+void ClientSubscriber::waypoint_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_waypoint(*data); }
 
-void ClientSubscriber::sign_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_float32_multi_array(*data); }
+void ClientSubscriber::sign_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_sign(*data); }
 
 void ClientSubscriber::message_sub_callback(const std_msgs::String::ConstPtr &data) { client.send_message(*data); }
