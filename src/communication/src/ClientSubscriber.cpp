e6cf6a05f42ee03d58d117121129ecaeebd749f9
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-04 14:24:40 -0500

    updates

--- src/communication/src/ClientSubscriber.cpp
+++ src/communication/src/ClientSubscriber.cpp
@@ -16,14 +16,20 @@ void ClientSubscriber::init() {
 	client.initialize();
 }
 
+// Utility.cpp l:202 Utility.cpp l:411
 void ClientSubscriber::road_object_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_road_object(*data); }
 
+// CameraNode.cpp l:135 CameraNode.cpp l:261
 void ClientSubscriber::camera_sub_callback(const sensor_msgs::Image::ConstPtr &data) { client.send_image_rgb(*data); }
 
+// CameraNode.cpp l:134 CameraNode.cpp l:260
 void ClientSubscriber::depth_sub_callback(const sensor_msgs::Image::ConstPtr &data) { client.send_image_depth(*data); }
 
+// Utility.cpp l:149 Controller.cpp l:826
 void ClientSubscriber::waypoint_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_waypoint(*data); }
 
+// SignFastest.hpp l:219 SignFastest.hpp l:477
 void ClientSubscriber::sign_sub_callback(const std_msgs::Float32MultiArray::ConstPtr &data) { client.send_sign(*data); }
 
+// Utility.hpp l:494 Utility.cpp l:48 
 void ClientSubscriber::message_sub_callback(const std_msgs::String::ConstPtr &data) { client.send_message(*data); }
