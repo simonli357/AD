b1b36ff940b181fa099947b1fec63986ffe0adc4
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-15 14:04:44 -0500

    updates

--- src/communication/include/service_calls/GoToSrvResponse.hpp
+++ src/communication/include/service_calls/GoToSrvResponse.hpp
@@ -1,14 +1,15 @@
 #include "std_msgs/Float32MultiArray.h"
+#include "ServiceCallResponse.hpp"
 
 using std_msgs::Float32MultiArray;
 
-class GoToSrvResponse {
+class GoToSrvResponse : public ServiceCallResponse {
   public:
 	GoToSrvResponse(Float32MultiArray state_refs, Float32MultiArray input_refs, Float32MultiArray wp_attributes, Float32MultiArray wp_normals);
 	GoToSrvResponse(GoToSrvResponse &&) = default;
 	GoToSrvResponse(const GoToSrvResponse &) = default;
-	GoToSrvResponse &operator=(GoToSrvResponse &&) = default;
-	GoToSrvResponse &operator=(const GoToSrvResponse &) = default;
+	GoToSrvResponse &operator=(GoToSrvResponse &&) = delete;
+	GoToSrvResponse &operator=(const GoToSrvResponse &) = delete;
 	~GoToSrvResponse() = default;
 
   private:
