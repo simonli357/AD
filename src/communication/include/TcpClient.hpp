38aa84a624a95bc65ac0615dc0a263d3da5a4134
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-16 15:57:36 -0500

    updates

--- src/communication/include/TcpClient.hpp
+++ src/communication/include/TcpClient.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "service_calls/SrvRequest.hpp"
 #include "std_msgs/Float32MultiArray.h"
 #include "std_msgs/String.h"
 #include <cstdint>
@@ -7,8 +8,13 @@
 #include <netinet/in.h>
 #include <queue>
 #include <sensor_msgs/Image.h>
+#include <sys/types.h>
 #include <thread>
 #include <vector>
+
+using sensor_msgs::Image;
+using std_msgs::Float32MultiArray;
+using std_msgs::String;
 
 class TcpClient {
   public:
@@ -22,15 +28,25 @@ class TcpClient {
 	~TcpClient();
 	// Methods
 	void initialize();
+	// Storage
 	std::queue<std::string> &get_strings();
+	std::queue<SrvRequest::GoToSrv> &get_go_to_srv_msgs();
+	std::queue<SrvRequest::GoToCmdSrv> &get_go_to_cmd_srv_msgs();
+	std::queue<SrvRequest::SetStatesSrv> &get_set_states_srv_msgs();
+	std::queue<SrvRequest::WaypointsSrv> &get_waypoints_srv_msgs();
+	// Encode
 	void send_type(const std::string &str);
 	void send_string(const std::string &str);
-	void send_image_rgb(const sensor_msgs::Image &img);
-	void send_image_depth(const sensor_msgs::Image &img);
-	void send_road_object(const std_msgs::Float32MultiArray &array);
-	void send_waypoint(const std_msgs::Float32MultiArray &array);
-	void send_sign(const std_msgs::Float32MultiArray &array);
-	void send_message(const std_msgs::String &msg);
+	void send_image_rgb(const Image &img);
+	void send_image_depth(const Image &img);
+	void send_road_object(const Float32MultiArray &array);
+	void send_waypoint(const Float32MultiArray &array);
+	void send_sign(const Float32MultiArray &array);
+	void send_message(const String &msg);
+	void send_go_to_srv(Float32MultiArray &state_refs, Float32MultiArray &input_refs, Float32MultiArray &wp_attributes, Float32MultiArray &wp_normals);
+	void send_go_to_cmd_srv(Float32MultiArray &state_refs, Float32MultiArray &input_refs, Float32MultiArray &wp_attributes, Float32MultiArray &wp_normals, bool success);
+	void send_set_states_srv(bool success);
+	void send_waypoints_srv(Float32MultiArray &state_refs, Float32MultiArray &input_refs, Float32MultiArray &wp_attributes, Float32MultiArray &wp_normals);
 
   private:
 	// Fields
@@ -45,8 +61,20 @@ class TcpClient {
 	std::thread receive;
 	std::map<uint8_t, std::function<void(TcpClient *, std::vector<uint8_t> &)>> data_actions;
 	std::vector<uint8_t> data_types;
+	// Storage
 	std::queue<std::string> strings;
+	std::queue<SrvRequest::GoToSrv> go_to_srv_msgs;
+	std::queue<SrvRequest::GoToCmdSrv> go_to_cmd_srv_msgs;
+	std::queue<SrvRequest::SetStatesSrv> set_states_srv_msgs;
+	std::queue<SrvRequest::WaypointsSrv> waypoints_srv_msgs;
 	// Methods
+	void set_data_types();
+	void set_data_actions();
 	void listen();
-	void parse_string(std::vector<uint8_t> &data);
+	// Decode
+	void parse_string(std::vector<uint8_t> &bytes);
+	void parse_go_to_srv(std::vector<uint8_t> &bytes);
+	void parse_go_to_cmd_srv(std::vector<uint8_t> &bytes);
+	void parse_set_states_srv(std::vector<uint8_t> &bytes);
+	void parse_waypoints_srv(std::vector<uint8_t> &bytes);
 };
