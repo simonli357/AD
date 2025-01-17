ec87daadd44d94860f2472d1f33f258687f41303
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-16 23:01:33 -0500

    updates

--- src/communication/include/service_calls/SrvRequest.hpp
+++ src/communication/include/service_calls/SrvRequest.hpp
@@ -14,35 +14,38 @@ class SrvRequest : public Decoder {
 	SrvRequest &operator=(const SrvRequest &) = delete;
 	~SrvRequest() = default;
 
-    struct GoToSrv {
-        std::string vrefName;
-        double x0;
-        double y0;
-        double yaw0;
-        double dest_x;
-        double dest_y;
-    };
+	struct GoToSrv {
+		std::string vrefName;
+		double x0;
+		double y0;
+		double yaw0;
+		double dest_x;
+		double dest_y;
+	};
 
-    struct GoToCmdSrv {
-        double dest_x;
-        double dest_y;
-    };
+	struct GoToCmdSrv {
+		double dest_x;
+		double dest_y;
+	};
 
-    struct SetStatesSrv {
-        double x;
-        double y;
-    };
+	struct SetStatesSrv {
+		double x;
+		double y;
+	};
 
-    struct WaypointsSrv {
-        std::string pathName;
-        std::string vrefName;
-        double x0;
-        double y0;
-        double yaw0;
-    };
+	struct WaypointsSrv {
+		std::string pathName;
+		std::string vrefName;
+		double x0;
+		double y0;
+		double yaw0;
+	};
 
-    GoToSrv parse_go_to_srv();
-    GoToCmdSrv parse_go_to_cmd_srv();
-    SetStatesSrv parse_set_states_srv();
-    WaypointsSrv parse_waypoints_srv();
+	GoToSrv parse_go_to_srv();
+	GoToCmdSrv parse_go_to_cmd_srv();
+	SetStatesSrv parse_set_states_srv();
+	WaypointsSrv parse_waypoints_srv();
+
+  private:
+	void double_from_bytes(double &value, std::vector<uint8_t> &bytes);
 };
