81aca41be25e5592ded3162d1ff7ca696260fb6a
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-20 23:12:25 -0500

    upates

--- src/communication/src/python_server/decoder.py
+++ src/communication/src/python_server/decoder.py
@@ -3,13 +3,13 @@ import struct
 
 def split(bytes):
     bytes_length = 4
-    lengths_length = struct.unpack('<I', bytes[:4])
-    num_elements = (lengths_length - bytes_length) / bytes_length
+    lengths_length = struct.unpack('<I', bytes[:bytes_length])[0]
+    num_elements = (lengths_length - bytes_length) // bytes_length
     splits = []
     size_offset = bytes_length
     data_offset = lengths_length
     for i in range(num_elements):
-        size = struct.unpack('<I', bytes[size_offset:size_offset + bytes_length])
+        size = struct.unpack('<I', bytes[size_offset:size_offset + bytes_length])[0]
         size_offset += bytes_length
         split = bytes[data_offset:data_offset + size]
         splits.append(split)
