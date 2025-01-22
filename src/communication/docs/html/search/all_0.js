d7614823ec135c1966e37e81725c2a9c16966545
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-22 14:20:54 -0500

    removed generated docs

--- src/communication/docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['connection_0',['Connection',['../classpython__server_1_1connection_1_1Connection.html',1,'python_server::connection']]]
-];
--- src/communication/docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['encoder_0',['Encoder',['../classEncoder.html',1,'']]]
-];
