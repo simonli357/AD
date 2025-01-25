7a3d6f22e120d3b46716fb5d46c607679c1f2db1
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2025-01-25 16:13:03 -0500

    update requirements.txt

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -562,7 +562,7 @@ void Utility::imu_callback(const sensor_msgs::Imu::ConstPtr& msg) {
     m_chassis.getRPY(roll, pitch, yaw);
 
     // if (real) yaw *= -1;
-    ROS_INFO("yaw: %.3f, pitch: %.3f", yaw * 180 / M_PI, pitch * 180 / M_PI);
+    // ROS_INFO("yaw: %.3f, pitch: %.3f", yaw * 180 / M_PI, pitch * 180 / M_PI);
     // ROS_INFO("yaw: %.3f, angular velocity: %.3f, acceleration: %.3f, %.3f, %.3f", yaw * 180 / M_PI, msg->angular_velocity.z, msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
     if (!imuInitialized) {
         imuInitialized = true;
