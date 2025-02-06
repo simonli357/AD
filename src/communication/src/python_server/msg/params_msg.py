7ce1de8a2fa398f40eb0318d6d0de4508fa2aab6
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-02-05 19:20:34 -0500

    updates

--- src/communication/src/python_server/msg/params_msg.py
+++ src/communication/src/python_server/msg/params_msg.py
@@ -12,13 +12,9 @@ class ParamsMsg:
         self.state_refs = deque()
         self.attributes = deque()
 
-    def decode_double_array(self, msg):
-        nparr = np.array(msg.data)
-        return nparr.reshape(-1, 1)
-
     def decode(self, bytes):
         splits = decoder.split(bytes)
         state_refs = Float64MultiArray().deserialize(splits[0])
         attributes = Float64MultiArray().deserialize(splits[1])
-        self.state_refs.append(self.decode_double_array(state_refs))
-        self.attributes.append(self.decode_double_array(attributes))
+        self.state_refs.append(np.array(state_refs.data).reshape(3, -1))
+        self.attributes.append(np.array(attributes.data))
