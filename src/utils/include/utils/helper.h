cad7ae477d08601e4ddd949c3e7c5723ceb58f86
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2025-02-14 16:51:14 -0500

    helper double to string with precision

--- src/utils/include/utils/helper.h
+++ src/utils/include/utils/helper.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
 namespace helper {
     std::string getSourceDirectory() {
         std::string file_path(__FILE__);  // __FILE__ is the full path of the source file
@@ -11,5 +15,11 @@ namespace helper {
             return file_path.substr(0, last_dir_sep);  // Extract directory path
         }
         return "";  // Return empty string if path not found
+    }
+
+    std::string d2str(double value, int precision = 3) {
+        std::ostringstream out;
+        out << std::fixed << std::setprecision(precision) << value;
+        return out.str();
     }
 }
