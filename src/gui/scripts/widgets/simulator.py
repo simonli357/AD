41a3cbab0521245e0db05c5f572bb18b01adb6d9
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-03-06 13:10:43 -0500

    updates

--- src/gui/scripts/widgets/simulator.py
+++ src/gui/scripts/widgets/simulator.py
@@ -10,8 +10,14 @@ class SimulatorWidget(QDialog):
         super().__init__()
         self.setMinimumSize(QtCore.QSize(800, 150))
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        data_dir = os.path.join(current_dir, 'data')
+        data_dir = os.path.join(current_dir, 'appdata')
         self.config = os.path.join(data_dir, 'simulator.yaml')
+
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+
+        if not os.path.isfile(self.config):
+            self.create_default_config()
 
         self.catkin_ws_cached = None
         self.args_cached = None
@@ -43,11 +49,22 @@ class SimulatorWidget(QDialog):
 
         self.setLayout(layout)
 
+    def create_default_config(self):
+        default_config = {
+            'catkin_ws': '/path/to/catkin_ws',
+            'args': 'run3.launch'
+        }
+        os.makedirs(os.path.dirname(self.config), exist_ok=True)
+        with open(self.config, 'w') as file:
+            yaml.dump(default_config, file)
+
     def accept(self):
+        src_ros = 'source /opt/ros/noetic/setup.sh'
+        src_devel = 'source devel/setup.bash'
         if not self.catkin_ws.text() or not self.args.text():
-            self.cmd = f'cd {self.catkin_ws_cached} && source devel/setup.bash && roslaunch sim_pkg {self.args_cached}'
+            self.cmd = f'{src_ros} && cd {self.catkin_ws_cached} && {src_devel} && roslaunch sim_pkg {self.args_cached}'
         else:
-            self.cmd = f'cd {self.catkin_ws.text()} && source devel/setup.bash && roslaunch sim_pkg {self.args.text()}'
+            self.cmd = f'{src_ros} && cd {self.catkin_ws.text()} && {src_devel} && roslaunch sim_pkg {self.args.text()}'
             self.cache['catkin_ws'] = self.catkin_ws.text()
             self.cache['args'] = self.args.text()
             with open(self.config, 'w') as file:
--- src/gui/scripts/widgets/terminal.py
+++ src/gui/scripts/widgets/terminal.py
@@ -15,29 +15,15 @@ class TerminalWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.main_window = self.parent()
-        self.current_terminal_type = TerminalType.ROS
+        self.kill_thread = None
+        self.terminals = deque()
+        self.terminals.append(TerminalType.ROS)
         self.message_history = deque([], 1000)
         self.sim_display = None
         self.sim_process = None
         self.sim_widget = SimulatorWidget()
         self.setup_ui()
         self.connect_signals()
-
-    def update_button_style(self, button, is_active):
-        """Update button color based on boolean state"""
-        color = "#ffa500" if is_active else "rgba(255, 255, 255, 0.08);"
-        button.setStyleSheet(f"""
-            QPushButton {{
-                background-color: {color};
-            }}
-            QPushButton:hover {{
-                background-color: #9933ff;
-            }}
-        """)
-
-    def update_buttons_style(self):
-        self.update_button_style(self.ros_btn, self.current_terminal_type == TerminalType.ROS)
-        self.update_button_style(self.sim_btn, self.current_terminal_type == TerminalType.SIM)
 
     def setup_ui(self) -> None:
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -46,8 +32,10 @@ class TerminalWidget(QtWidgets.QWidget):
         buttons = QWidget()
         self.button_wrapper = QtWidgets.QHBoxLayout(buttons)
         self.buttons = deque()
+        self.stop_btn = QtWidgets.QPushButton(' Stop')
         self.ros_btn = QtWidgets.QPushButton(' ROS Debug')
         self.sim_btn = QtWidgets.QPushButton('󰘨 Simulator')
+        self.buttons.append(self.stop_btn)
         self.buttons.append(self.ros_btn)
         self.buttons.append(self.sim_btn)
         for btn in self.buttons:
@@ -86,32 +74,90 @@ class TerminalWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.stacked_widget)
 
         self.update_buttons_style()
+        self.stop_btn.setStyleSheet("""
+            QPushButton {
+                background-color: #ff0000;
+            }
+            QPushButton:hover {
+                background-color: #ff4d4d;
+            }
+        """)
 
     def connect_signals(self):
+        self.stop_btn.clicked.connect(self.handle_stop_btn_click)
         self.ros_btn.clicked.connect(self.handle_ros_btn_click)
         self.sim_btn.clicked.connect(self.handle_sim_btn_click)
+
+    def update_button_style(self, button, is_active):
+        """Update button color based on boolean state"""
+        color = "#ffa500" if is_active else "rgba(255, 255, 255, 0.08);"
+        button.setStyleSheet(f"""
+            QPushButton {{
+                background-color: {color};
+            }}
+            QPushButton:hover {{
+                background-color: #9933ff;
+            }}
+        """)
+
+    def update_buttons_style(self):
+        current_terminal_type = self.get_current_terminal_type()
+        self.update_button_style(self.ros_btn, current_terminal_type == TerminalType.ROS)
+        self.update_button_style(self.sim_btn, current_terminal_type == TerminalType.SIM)
+
+    def find_widget_index(self, term_type):
+        if term_type not in self.terminals:
+            self.terminals.append(term_type)
+            widget_index = len(self.terminals) - 1
+        else:
+            widget_index = self.terminals.index(term_type)
+        return widget_index
+
+    def set_terminal(self, term_type):
+        widget_index = self.find_widget_index(term_type)
+        self.stacked_widget.setCurrentIndex(widget_index)
+        self.update_buttons_style()
+
+    def get_current_terminal_type(self):
+        widget_index = self.stacked_widget.currentIndex()
+        return self.terminals[widget_index]
+
+    def kill_process(self, process, button, display, terminal_type):
+        process.terminate()
+        process.waitForFinished()
+        self.stacked_widget.removeWidget(display)
+        display.deleteLater()
+        self.terminals.remove(terminal_type)
+        self.stacked_widget.setCurrentIndex(0)
+        self.update_buttons_style()
+
+    ################
+    # Stop Button
+    ################
+
+    def handle_stop_btn_click(self):
+        current_terminal_type = self.get_current_terminal_type()
+        if current_terminal_type == TerminalType.SIM:
+            self.stop_sim_process()
 
     ################
     # Simulator
     ################
 
     def handle_sim_btn_click(self):
-        if self.current_terminal_type == TerminalType.SIM:
+        current_terminal_type = self.get_current_terminal_type()
+        if current_terminal_type == TerminalType.SIM:
             return
         if self.sim_display is None:
             self.sim_widget.exec()
             cmd = self.sim_widget.get_cmd()
             if cmd is None:
                 return
-            self.current_terminal_type = TerminalType.SIM
-            self.update_buttons_style()
             self.create_sim_display()
-            self.stacked_widget.setCurrentIndex(1)
+            self.set_terminal(TerminalType.SIM)
             self.start_sim_process(cmd)
         else:
-            self.current_terminal_type = TerminalType.SIM
-            self.stacked_widget.setCurrentIndex(1)
-            self.update_buttons_style()
+            self.set_terminal(TerminalType.SIM)
 
     def create_sim_display(self):
         self.sim_display = QtWidgets.QTextEdit()
@@ -134,6 +180,16 @@ class TerminalWidget(QtWidgets.QWidget):
         self.sim_process.readyReadStandardOutput.connect(self.read_sim_output)
         self.sim_process.start('bash', ['-i', '-c', cmd])
 
+    def stop_sim_process(self):
+        if hasattr(self, 'sim_process') and self.sim_process:
+            try:
+                self.sim_process.readyReadStandardOutput.disconnect()
+            except Exception as e:
+                print(e)
+            self.kill_process(self.sim_process, self.sim_btn, self.sim_display, TerminalType.SIM)
+            self.sim_process = None
+            self.sim_display = None
+
     def read_sim_output(self):
         data = self.sim_process.readAllStandardOutput().data().decode()
         self.sim_display.append(data.strip())
@@ -143,9 +199,9 @@ class TerminalWidget(QtWidgets.QWidget):
     ################
 
     def handle_ros_btn_click(self):
-        if self.current_terminal_type == TerminalType.ROS:
+        current_terminal_type = self.get_current_terminal_type()
+        if current_terminal_type == TerminalType.ROS:
             return
-        self.current_terminal_type = TerminalType.ROS
         self.stacked_widget.setCurrentIndex(0)
         self.update_buttons_style()
 
