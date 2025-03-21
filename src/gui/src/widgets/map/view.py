3ceac19d7891a0a958b7bfe5131c67a52e0bb696
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-03-21 00:06:39 -0400

    updates

--- src/gui/src/widgets/map/view.py
+++ src/gui/src/widgets/map/view.py
@@ -215,8 +215,8 @@ class GraphicsView(QGraphicsView):
     def calculate_total_path_distance(self):
         if self.map_widget.state_refs_np.shape[1] < 2:
             return 0.0
-        x_coords = self.state_refs_np[0, :]
-        y_coords = self.state_refs_np[1, :]
+        x_coords = self.map_widget.state_refs_np[0, :]
+        y_coords = self.map_widget.state_refs_np[1, :]
         dx = x_coords[1:] - x_coords[:-1]
         dy = y_coords[1:] - y_coords[:-1]
         distances = np.sqrt(dx**2 + dy**2)
