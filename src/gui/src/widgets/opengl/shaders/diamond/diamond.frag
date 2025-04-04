4031ec408b51482ab0591c5c71c302581f2c3d3c
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-04 17:07:10 -0400

    updates

--- src/gui/src/widgets/opengl/shaders/diamond/diamond.frag
+++ src/gui/src/widgets/opengl/shaders/diamond/diamond.frag
@@ -4,6 +4,5 @@ out vec4 FragColor;
 
 void main()
 {
-    // FragColor = vec4(Color, 1.0);
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red
+    FragColor = vec4(Color, 1.0);
 }
--- src/gui/src/widgets/opengl/waypoints.py
+++ src/gui/src/widgets/opengl/waypoints.py
@@ -8,11 +8,8 @@ import glm
 
 class WaypointsRenderer:
     def __init__(self):
-        self.vao = None
-        self.base_vbo = None
-        self.instance_vbo = None
         self.num_instances = 0
-
+        self.vao = gl.glGenVertexArrays(1)
         # Diamond geometry (2 triangles)
         self.base_vertices = np.array([
             # Positions (3D for proper matrix transformations)
@@ -23,6 +20,33 @@ class WaypointsRenderer:
             [0.5, 0.0, 0.1],  # Right
             [0.0, -0.5, 0.1],  # Bottom
         ], dtype=np.float32).flatten()
+        self.base_vbo = vbo.VBO(self.base_vertices)
+        self.instance_vbo = vbo.VBO(np.array([], dtype=np.float32), usage=gl.GL_DYNAMIC_DRAW)
+
+        # Permanent VAO setup
+        gl.glBindVertexArray(self.vao)
+
+        # Base vertex attributes (position)
+        self.base_vbo.bind()
+        gl.glEnableVertexAttribArray(0)
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+
+        # Instance vertex attributes (color + matrix)
+        self.instance_vbo.bind()
+        # Color attribute
+        gl.glEnableVertexAttribArray(1)
+        gl.glVertexAttribPointer(1, 4, gl.GL_FLOAT, gl.GL_FALSE, 20 * 4, None)
+        gl.glVertexAttribDivisor(1, 1)
+
+        # Matrix columns
+        for i in range(4):
+            gl.glEnableVertexAttribArray(2 + i)
+            gl.glVertexAttribPointer(2 + i, 4, gl.GL_FLOAT, gl.GL_FALSE, 20 * 4, gl.ctypes.c_void_p(16 + i * 16))
+            gl.glVertexAttribDivisor(2 + i, 1)
+
+        gl.glBindVertexArray(0)
+        self.base_vbo.unbind()
+        self.instance_vbo.unbind()
 
         self.ATTRIBUTES = {  # (Same color definitions as before)
             0: (1.0, 1.0, 0.0),   # Yellow
@@ -52,48 +76,6 @@ class WaypointsRenderer:
 
         return world_x, world_y
 
-    # def get_gl_coords(self, real_x, real_y):
-    #     # Map to [-1, 1] range
-    #     gl_x = (2 * real_x / MapData.REAL_WORLD_WIDTH.value) - 1
-    #     gl_y = (2 * real_y / MapData.REAL_WORLD_HEIGHT.value) - 1
-    #     return gl_x, gl_y
-
-    def setup_vao(self, instance_array):
-        self.vao = gl.glGenVertexArrays(1)
-        gl.glBindVertexArray(self.vao)
-
-        # Base vertex data (diamond shape)
-        self.base_vbo = vbo.VBO(self.base_vertices)
-        self.base_vbo.bind()
-
-        gl.glEnableVertexAttribArray(0)
-        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * 4, gl.ctypes.c_void_p(0))
-
-        self.base_vbo.unbind()
-
-        # Instance VBO (color + model matrix)
-        if self.instance_vbo is not None:
-            self.instance_vbo.delete()
-
-        self.instance_vbo = vbo.VBO(instance_array)
-        self.instance_vbo.bind()
-
-        # Color attribute (vec3)
-        gl.glEnableVertexAttribArray(1)
-        gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 19 * 4, gl.ctypes.c_void_p(0))
-        gl.glVertexAttribDivisor(1, 1)
-
-        # Model matrix attributes (4 vec4s)
-        for i in range(4):
-            gl.glEnableVertexAttribArray(2 + i)
-            offset = 12 + i * 16  # 3 floats (color) * 4 + i * 16
-            gl.glVertexAttribPointer(2 + i, 4, gl.GL_FLOAT, gl.GL_FALSE, 19 * 4, gl.ctypes.c_void_p(offset))
-            gl.glVertexAttribDivisor(2 + i, 1)
-
-        self.instance_vbo.unbind()
-
-        gl.glBindVertexArray(0)
-
     def update_waypoints(self, state_refs_np, attributes_np, widget_width, widget_height):
         if state_refs_np is None or state_refs_np.size == 0:
             self.num_instances = 0
@@ -115,12 +97,24 @@ class WaypointsRenderer:
             model_data = glm.value_ptr(model)
 
             # Add color and matrix data
-            instance_data.extend(color)
+            instance_data.extend([color[0], color[1], color[2], 1.0])
             instance_data.extend(model_data[:16])
 
         instance_array = np.array(instance_data, dtype=np.float32)
-        print("First instance data:", instance_array[:19])
-        self.setup_vao(instance_array.flatten())
+
+        # Update VBO data without recreating objects
+        self.instance_vbo.set_array(instance_array)
+        self.num_instances = state_refs_np.shape[1]
+
+        # Ensure proper buffer binding
+        self.instance_vbo.bind()
+        gl.glBufferData(
+            gl.GL_ARRAY_BUFFER,
+            instance_array.nbytes,
+            instance_array,
+            gl.GL_DYNAMIC_DRAW
+        )
+        self.instance_vbo.unbind()
 
     def draw(self, projection, view):
         if self.num_instances == 0:
