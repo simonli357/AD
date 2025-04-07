32df5d3a57a06565f48443261d1dc427dcdaa96e
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-07 16:47:43 -0400

    updates

--- src/gui/src/widgets/car/car.py
+++ src/gui/src/widgets/car/car.py
@@ -105,28 +105,52 @@ class CarWidget(QtWidgets.QOpenGLWidget):
             proj_matrix=self.proj_mat
         )
 
-        self.draw_detected_objects(self.main_window.map_widget.detected_data, self.main_window.map_widget.road_msg_dict, self.main_window.map_widget.object_dict)
+        self.draw_gt()
 
-    def draw_detected_objects(self, detected_data, road_msg_dict, object_dict):
-        if detected_data is None or len(detected_data) == 0:
-            return
-        for i in range(len(detected_data)):
-            obj_type = detected_data[i, road_msg_dict['type']]
-            x_real = detected_data[i, road_msg_dict['x']]
-            y_real = detected_data[i, road_msg_dict['y']]
-            orientation = detected_data[i, road_msg_dict['orientation']]
+        # self.draw_detected_objects(self.main_window.map_widget.detected_data, self.main_window.map_widget.road_msg_dict, self.main_window.map_widget.object_dict)
 
-            # Convert map coordinates to pixel coordinates
-            x, y = self.get_gl_coords(x_real, MapData.REAL_WORLD_HEIGHT.value - y_real)
+    # def draw_detected_objects(self, detected_data, road_msg_dict, object_dict):
+    #     if detected_data is None or len(detected_data) == 0:
+    #         return
+    #     for i in range(len(detected_data)):
+    #         obj_type = detected_data[i, road_msg_dict['type']]
+    #         x_real = detected_data[i, road_msg_dict['x']]
+    #         y_real = detected_data[i, road_msg_dict['y']]
+    #         orientation = detected_data[i, road_msg_dict['orientation']]
+
+    #         # Convert map coordinates to pixel coordinates
+    #         x, y = self.get_gl_coords(x_real, MapData.REAL_WORLD_HEIGHT.value - y_real)
+    #         # orientation = 2 * np.pi - orientation
+    #         orientation = - orientation
+
+    #         if object_dict[obj_type] == 'Car' and i == 0:
+    #             continue
+    #         elif object_dict[obj_type] == 'Car':
+    #             self.shader_renderer.draw_car(x, y, orientation, NamedColor.RED, 0.55, self.view_mat, self.proj_mat)
+    #         else:
+    #             self.shader_renderer.draw_road_object(object_dict[obj_type], x, y, orientation, 16.0, self.view_mat, self.proj_mat)
+
+    def draw_gt(self):
+        for index, row in self.main_window.map_widget.data.iterrows():
+            entity_type, orientation = row['Type'], row['Orientation']
+
+            x, y = self.get_gl_coords(row['X'], MapData.REAL_WORLD_HEIGHT.value - row['Y'])
+
             # orientation = 2 * np.pi - orientation
             orientation = - orientation
 
-            if object_dict[obj_type] == 'Car' and i == 0:
-                continue
-            elif object_dict[obj_type] == 'Car':
-                self.shader_renderer.draw_car(x, y, orientation, NamedColor.RED, 0.55, self.view_mat, self.proj_mat)
+            if entity_type == 'Car':
+                self.shader_renderer.draw_car(x, y, -orientation, NamedColor.BLUE, 0.55, self.view_mat, self.proj_mat)
+            elif entity_type == 'Destination':
+                self.shader_renderer.draw_circle(
+                    center=(x, y),
+                    radius=8.0,
+                    color=(0.0, 0.7, 0.7, 0.7),
+                    view_matrix=self.view_mat,
+                    proj_matrix=self.proj_mat
+                )
             else:
-                self.shader_renderer.draw_road_object(object_dict[obj_type], x, y, orientation, 16.0, self.view_mat, self.proj_mat)
+                self.shader_renderer.draw_road_object(entity_type, x, y, orientation, 16.0, self.view_mat, self.proj_mat)
 
     def render_text(self, text, size, color: (int, int, int, int), x, y) -> None:
         painter = QPainter(self)
