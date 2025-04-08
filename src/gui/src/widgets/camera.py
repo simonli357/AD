8c186037331be1b389fb0c90fd409738fd0318b4
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-08 18:23:55 -0400

    updates

--- src/gui/src/widgets/camera.py
+++ src/gui/src/widgets/camera.py
@@ -45,8 +45,8 @@ class CameraWidget(QtWidgets.QWidget):
         container_layout.setContentsMargins(0, 0, 0, 0)
         container_layout.setAlignment(QtCore.Qt.AlignCenter)
         self.camera_label = QtWidgets.QLabel(container_widget)
-        self.camera_label.setFixedWidth(CameraParams.MIN_WIDTH.value)
-        self.camera_label.setFixedHeight(CameraParams.MIN_HEIGHT.value)
+        self.camera_label.setMinimumWidth(CameraParams.MIN_WIDTH.value)
+        self.camera_label.setMinimumWidth(CameraParams.MIN_HEIGHT.value)
         self.camera_label.setAlignment(QtCore.Qt.AlignCenter)
         self.camera_label.setStyleSheet("""
             background-color: transparent;
