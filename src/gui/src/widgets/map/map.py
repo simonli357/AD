33250ac8f433143b1de7ee0f2ea2c00645c18dfe
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-10 18:43:10 -0400

    updates

--- src/gui/src/widgets/map/map.py
+++ src/gui/src/widgets/map/map.py
@@ -377,7 +377,7 @@ class MapWidget(QtWidgets.QOpenGLWidget):
         angle = 0
         for i in range(0, len(self.waypoints) - 1, 4):
             if i + 3 > len(self.waypoints):
-                self.shader_renderer.draw_triangle(x1, y1, 0, angle, (4, 4), (1.0, 1.0, 0.0, 1.0), self.view_mat, self.proj_mat)
+                self.shader_renderer.draw_triangle(x1, y1, 1.0, angle, (4, 4), (1.0, 1.0, 0.0, 1.0), self.view_mat, self.proj_mat)
             else:
                 x2, y2 = self.get_gl_coords(self.waypoints[i + 2], self.waypoints[i + 3])
                 dx = x2 - x1
