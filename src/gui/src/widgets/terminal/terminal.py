66389de7467a1f6c4009414f8cd6d1de9787c768
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-11 13:24:39 -0400

    updates

--- src/gui/src/widgets/terminal/terminal.py
+++ src/gui/src/widgets/terminal/terminal.py
@@ -332,8 +332,8 @@ class TerminalWidget(QtWidgets.QWidget):
                     val = int(match.group(1))
                     if self.compile_progress is not None:
                         self.compile_progress.set_progress(val)
-                    if '#### Running command: "make -j16 -l16"' in line:
-                        self.compile_progress.end()
+                if '#### Running command: "make -j16 -l16"' in line:
+                    self.compile_progress.end()
                 else:
                     self.compile_progress.increment(2)
 
