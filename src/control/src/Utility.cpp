96615451be73e20d2113ecaf1f9a3f8cc5e91cf8
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-12 16:30:56 -0400

    updates

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -102,8 +102,6 @@ void Utility::fetch_run_params(double &x_init, double &y_init, double &yaw_init)
     double x = msg_ptr->pose.pose.position.x;
     double y = msg_ptr->pose.pose.position.y;
     double z = msg_ptr->pose.pose.position.z;
-    auto q1 = tf2::Quaternion(msg_ptr->pose.pose.orientation.x, msg_ptr->pose.pose.orientation.y, msg_ptr->pose.pose.orientation.z, msg_ptr->pose.pose.orientation.w);
-    double yaw = tf2::impl::getYaw(q1);
 
     std::vector<std::array<std::any, 5>> runs_with_info;
 
