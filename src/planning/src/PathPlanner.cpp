f55a7b2f381e0a722c633279f411e09126cc82f0
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-15 14:48:37 -0400

    updates

--- src/planning/src/PathPlanner.cpp
+++ src/planning/src/PathPlanner.cpp
@@ -68,8 +68,8 @@ void PathPlanner::construct_path() {
 	}
     /* path_utils.distance_filter(general_path, distance_threshold, hw_density_factor, cw_density_factor); */
 	condensed_path = spline_utils.interpolate_path(general_path, density, hw_density_factor, cw_density_factor);
-    /* path_utils.compute_speeds(condensed_path, vref, density, hw_density_factor, cw_density_factor); */
     /* path_utils.yaw_filter(condensed_path, yaw_threshold); */
+    path_utils.compute_speeds(condensed_path, vref, density, hw_density_factor, cw_density_factor);
 }
 
 void PathPlanner::plan_path(Float32MultiArray &out_state_refs, Float32MultiArray &out_input_refs, Float32MultiArray &out_attributes, Float32MultiArray &out_normals) {
