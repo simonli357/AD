a71f04acb112c53497764325d1833785e7ccb9f6
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-16 22:59:54 -0400

    updates

--- src/gui/src/widgets/map/map.py
+++ src/gui/src/widgets/map/map.py
@@ -208,9 +208,10 @@ class MapWidget(QtWidgets.QOpenGLWidget):
 
         if self.show_destinations:
             self.destinations_renderer.draw((0.0, 0.7, 0.7, 1.0), self.proj_mat, self.view_mat)
-            for x, y in self.visited:
-                print(x, y)
-                self.shader_renderer.draw_circle(x, y, 8.0, (0.0, 1.0, 0.0, 1.0), self.view_mat, self.proj_mat)
+
+        for x, y in self.visited:
+            print(x, y)
+            self.shader_renderer.draw_circle(x, y, 8.0, (0.0, 1.0, 0.0, 1.0), self.view_mat, self.proj_mat)
 
         for index, row in self.data.iterrows():
             entity_type, orientation = row['Type'], row['Orientation']
