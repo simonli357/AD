71806693956c4132ae5b4089bfe8511b2a9762b7
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-18 08:45:46 -0400

    updates

--- src/communication/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-    packages=['python_server', 'python_server.service_calls', 'python_server.msg'],
-    package_dir={'': 'src'}
-)
-
-setup(**setup_args)
