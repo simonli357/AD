78521bc791f8954a62cf51d7eeb633876f64497b
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-19 16:03:14 -0400

    updates

--- src/planning/src/PathPlanner.cpp
+++ src/planning/src/PathPlanner.cpp
@@ -91,7 +91,6 @@ void PathPlanner::plan_path(Float32MultiArray &out_state_refs, Float32MultiArray
 		out_attributes.data.push_back(static_cast<double>(v.attribute));
 
 		// Normals
-		out_normals.data.push_back(std::cos(v.normal_angle));
-		out_normals.data.push_back(std::sin(v.normal_angle));
+		out_normals.data.push_back(v.normal_angle);
 	}
 }
