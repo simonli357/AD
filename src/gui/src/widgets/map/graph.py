c197122170748a4616aa97c73f8c2fec885bf954
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-25 12:25:39 -0400

    updates

--- src/gui/src/widgets/map/graph.py
+++ src/gui/src/widgets/map/graph.py
@@ -1,7 +1,7 @@
 from ..opengl.shader import ShaderRenderer
 from ..opengl.renderer import InstanceRenderer
 from ..opengl.utils import create_shader_program, shader_path
-from ..enums import OpenGLContextName, NamedColor
+from ..enums import OpenGLContextName
 from ..opengl.instance.arrows import ArrowInstanceRenderer
 from PyQt5.QtCore import Qt
 
@@ -17,13 +17,14 @@ class InstanceData:
         self.starts = []
         self.ends = []
         self.edge_pairs = []
+        self.colors = []
 
 
 class Shapes:
     def __init__(self):
         self.node_shader = create_shader_program(shader_path('node', 'node.vert'), shader_path('node', 'node.frag'))
         self.node_default_scale = 8.0
-        self.arrow_thickness = 15.0
+        self.arrow_thickness = 16.0
         self.node_base_vertices = np.array([
             # Positions (3D for proper matrix transformations)
             [0.0, 0.5, 0.1],  # Top
@@ -49,6 +50,19 @@ class GraphEditor:
         self._dragging = False
         self._drag_index = None
 
+        self.ATTRIBUTES_COLORS = {
+            0: (1.0, 1.0, 0.0, 1.0),
+            1: (0.0, 1.0, 0.0, 1.0),
+            2: (1.0, 0.0, 0.0, 1.0),
+            3: (1.0, 0.5, 0.0, 1.0),
+            4: (0.5, 0.0, 0.5, 1.0),
+            5: (0.8, 0.7, 1.0, 1.0),
+            6: (1.0, 1.0, 1.0, 1.0),
+            7: (0.0, 1.0, 1.0, 1.0),
+            8: (0.4, 0.5, 0.7, 1.0),
+            9: (0.5, 0.0, 0.5, 1.0),
+        }
+
     def draw(self, proj_mat, view_mat):
         self.proj_mat = proj_mat
         self.view_mat = view_mat
@@ -61,8 +75,8 @@ class GraphEditor:
 
         if len(self.instance_data.positions) == 0:
             self.update_instance_data()
-            self.node_instance_renderer = InstanceRenderer(self.shapes.node_base_vertices, self.instance_data.positions, NamedColor.INDIGO.value, scales=self.shapes.node_default_scale, shader_program=self.shapes.node_shader)
-            self.arrow_instance_renderer = ArrowInstanceRenderer(self.instance_data.starts, self.instance_data.ends, color=(1, 0, 0, 1), thickness=self.shapes.arrow_thickness, edge_pairs=self.instance_data.edge_pairs)
+            self.node_instance_renderer = InstanceRenderer(self.shapes.node_base_vertices, self.instance_data.positions, self.instance_data.colors, scales=self.shapes.node_default_scale, shader_program=self.shapes.node_shader)
+            self.arrow_instance_renderer = ArrowInstanceRenderer(self.instance_data.starts, self.instance_data.ends, color=(0.0, 0.7, 0, 1), thickness=self.shapes.arrow_thickness, edge_pairs=self.instance_data.edge_pairs)
         if self.node_instance_renderer is not None:
             self.node_instance_renderer.render(proj_mat, view_mat)
         if self.arrow_instance_renderer is not None:
@@ -86,10 +100,10 @@ class GraphEditor:
         if nearest != self.prev_hovered:
             # restore previous
             if self.prev_hovered is not None:
-                self.node_instance_renderer.color_instance(self.prev_hovered, NamedColor.INDIGO.value)
-            # hide new
+                self.node_instance_renderer.scale_instance(self.prev_hovered, self.shapes.node_default_scale)
+            # grow selected
             if nearest is not None:
-                self.node_instance_renderer.color_instance(nearest, NamedColor.YELLOW.value)
+                self.node_instance_renderer.scale_instance(nearest, self.shapes.node_default_scale * 1.5)
             self.prev_hovered = nearest
 
     def update_instance_data(self):
@@ -97,11 +111,13 @@ class GraphEditor:
             int_id = int(node_id.lstrip('n'))
             x_real = float(data.get('x', 0))
             y_real = float(data.get('y', 0))
+            attr = int(data.get('attr', 0))
             x, y = self.map_widget.get_gl_coords(x_real, y_real)
             z = 0.0
             self.instance_data.keys.append(node_id)
             self.instance_data.ids.append(int_id)
             self.instance_data.positions.append((x, y, z))
+            self.instance_data.colors.append(self.ATTRIBUTES_COLORS[attr])
 
         id2pos = {
             nid: pos
@@ -160,7 +176,7 @@ class GraphEditor:
 
             # tell the instancer to move it
             self.node_instance_renderer.translate_instance(self._drag_index, x_gl, y_gl)
-            self.node_instance_renderer.color_instance(self._drag_index, NamedColor.YELLOW.value)
+            self.node_instance_renderer.scale_instance(self._drag_index, self.shapes.node_default_scale * 1.5)
             node_id = self.instance_data.ids[self._drag_index]
             self.arrow_instance_renderer.update_for_node(
                 node_id,
@@ -171,14 +187,9 @@ class GraphEditor:
 
     def mouseReleaseEventNonDrag(self, event):
         if event.button() == Qt.LeftButton:
+            if self._drag_index is not None:
+                self.node_instance_renderer.scale_instance(self._drag_index, self.shapes.node_default_scale)
             self._dragging = False
-            xw, yw = self.map_widget.get_real_world_coords(event.x(), event.y())
-            if self._drag_index is None:
-                return
-            node_key = self.instance_data.keys[self._drag_index]
-            self.G.nodes[node_key]['x'] = float(xw)
-            self.G.nodes[node_key]['y'] = float(yw)
-            self.node_instance_renderer.color_instance(self._drag_index, NamedColor.INDIGO.value)
             self._drag_index = None
 
     def mouseReleaseEventDragging(self, event):
