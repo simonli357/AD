f51191bfa2923b06e83bfe53bd3ddfb1ca899048
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-25 20:43:40 -0400

    updates

--- src/planning/src/map/Track.cpp
+++ src/planning/src/map/Track.cpp
@@ -17,10 +17,10 @@ std::istream &operator>>(std::istream &in, Track::ATTRIBUTE &attr) {
 }
 
 Track::Track() {
-	read_graph_xml();
-	/* read_graph(); */
-	/* adjust_graph(); */
-	/* compute_edge_distances(); */
+	/* read_graph_xml(); */
+	read_graph();
+	adjust_graph();
+	compute_edge_distances();
 }
 
 void Track::read_graph_xml() {
