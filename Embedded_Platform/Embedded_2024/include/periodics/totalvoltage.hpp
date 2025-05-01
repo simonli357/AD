e5f86f2e7a3727cf0649134e923b34706eedb3fd
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2025-05-01 11:38:00 -0400

    remove useless comments

--- Embedded_Platform/Embedded_2024/include/periodics/totalvoltage.hpp
+++ Embedded_Platform/Embedded_2024/include/periodics/totalvoltage.hpp
@@ -1,43 +1,8 @@
-/**
- * Copyright (c) 2019, Bosch Engineering Center Cluj and BFMC organizers
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
-
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
-
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
-
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
-
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-*/
-
-
-/* Include guard */
 #ifndef TOTALVOLTAGE_HPP
 #define TOTALVOLTAGE_HPP
 
-/* The mbed library */
 #include <mbed.h>
-/* Header file for the task manager library, which  applies periodically the fun function of it's children*/
 #include <utils/task.hpp>
-
 
 namespace periodics
 {
@@ -69,7 +34,7 @@ namespace periodics
             bool                m_isActive;
 
             float m_median;
-    }; // class CTotalVoltage
-}; // namespace periodics
+    };
+};
 
 #endif // TOTALVOLTAGE_HPP
