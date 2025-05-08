ee6e3eea6c0e74a29f2c21563ab5ec3cec8289cf
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-08 17:28:31 -0400

    updates

--- src/control/src/Controller.cpp
+++ src/control/src/Controller.cpp
@@ -54,7 +54,7 @@ public:
         utils.debug("state machine initialized", 2);
         db.graph_queries->set_graph(PathManager::path_planner.serialized_graph);
 
-        ros::Subscriber sub = nh.subscribe("/gazebo/model_states", 1, &StateMachine::model_states_callback, this);
+        model_states = nh.subscribe("/gazebo/model_states", 1, &StateMachine::model_states_callback, this);
         
         // set callbacks for tcp client
         utils.tcp_client->set_send_run_callback(
@@ -111,6 +111,7 @@ public:
         // utils.stop_car();
     }
     ros::NodeHandle& nh;
+    ros::Subscriber model_states;
 
     bool initialized = false;
     bool wait_for_green_flag = false;
