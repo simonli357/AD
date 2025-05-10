1a812042686e65944af4862c5f67aaaffffefcf5
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-10 17:21:43 -0400

    updates

--- src/gui/src/main.py
+++ src/gui/src/main.py
@@ -187,7 +187,7 @@ class MainWindow(QMainWindow):
         self.server.tcp_client.on_set_states = self.comm.set_states_signal.emit
         self.server.tcp_client.on_params = self.comm.params_signal.emit
         self.server.tcp_client.on_waypoint = self.comm.waypoints_signal.emit
-        while (not self.server.tcp_client.tcp_can_send):
+        while (not self.server.tcp_client.run_can_send):
             time.sleep(0.2)
             continue
         self.server.tcp_client.refresh_run()
