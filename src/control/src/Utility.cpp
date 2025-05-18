77950d13a1e2906b3f86956f740d2ae2b92a47a5
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-18 19:12:00 -0400

    updates

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -139,7 +139,7 @@ void Utility::fetch_run_params() {
     this->pathName = "gps";
 
     constexpr size_t TARGET_SAMPLES = 50;
-    constexpr double MAX_ACCEPTABLE_STD = 0.03;
+    constexpr double MAX_ACCEPTABLE_STD = 0.01;
     constexpr double CLUSTER_RADIUS = 0.2;
     constexpr size_t MIN_CLUSTER_SIZE = 5;
 
