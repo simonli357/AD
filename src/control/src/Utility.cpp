60397df603b884526b8a0d6df107da733c9c5814
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-18 19:23:54 -0400

    updates

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -135,9 +135,9 @@ void Utility::fetch_run_params() {
     this->pathName = "gps";
 
     constexpr size_t TARGET_SAMPLES = 50;
-    constexpr double MAX_ACCEPTABLE_STD = 0.2;
-    constexpr double CLUSTER_RADIUS = 0.1;
-    constexpr size_t MIN_CLUSTER_SIZE = 5;
+    constexpr double MAX_ACCEPTABLE_STD = 0.25;
+    constexpr double CLUSTER_RADIUS = 0.3;
+    constexpr size_t MIN_CLUSTER_SIZE = 12;
 
     std::vector<geometry_msgs::Point> samples;
     samples.reserve(TARGET_SAMPLES);
