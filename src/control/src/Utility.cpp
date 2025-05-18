bb4876dfa5ebd83f16f9d27c1c733fe1173610b1
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-18 18:33:56 -0400

    updates

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -67,7 +67,7 @@ void Utility::fetch_run_params() {
 
 	this->pathName = "gps";
 
-	const size_t sample_count = 400;
+	const size_t sample_count = 20;
 	std::vector<geometry_msgs::PoseWithCovarianceStamped::ConstPtr> samples;
 	samples.reserve(sample_count);
 
