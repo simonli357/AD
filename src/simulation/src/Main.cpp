fa9abc55fac8a0d6569ab2f79da6cdce05a702eb
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-19 16:33:25 -0400

    updates

--- src/simulation/src/Main.cpp
+++ src/simulation/src/Main.cpp
@@ -1,6 +1,7 @@
+#include "HighwayManager.hpp"
+#include "RoundaboutManager.hpp"
 #include "SpeedCurveManager.hpp"
 #include "TrafficManager.hpp"
-#include "HighwayManager.hpp"
 #include <gazebo_msgs/SetModelState.h>
 #include <ros/init.h>
 #include <ros/node_handle.h>
@@ -15,10 +16,11 @@ int main(int argc, char **argv) {
 
 	TrafficManager *traffic_manager = nullptr;
 	HighwayManager *highway_manager = nullptr;
-    SpeedCurveManager *speed_curve_manager = nullptr;
+	SpeedCurveManager *speed_curve_manager = nullptr;
+	RoundaboutManager *roundabout_manager = nullptr;
 
 	std::string run_type = "traffic";
-    double car_speed = 0.1;
+	double car_speed = 0.1;
 
 	if (!nh.getParam("/run_type", run_type)) {
 		std::cout << "Missing param: run_type" << std::endl;
@@ -31,37 +33,26 @@ int main(int argc, char **argv) {
 
 	if (run_type == "trafic") {
 		TrafficManager manager(nh, tele);
-        traffic_manager = &manager;
-        traffic_manager->initialize();
+		traffic_manager = &manager;
+		traffic_manager->initialize();
 	} else if (run_type == "highway") {
-        HighwayManager manager(nh, tele, car_speed);
-        highway_manager = &manager;
-        highway_manager->initialize();
+		HighwayManager manager(nh, tele, car_speed);
+		highway_manager = &manager;
+		highway_manager->initialize();
 	} else if (run_type == "speedcurve") {
-        SpeedCurveManager manager(nh, tele, car_speed);
-        speed_curve_manager = &manager;
-        speed_curve_manager->initialize();
+		SpeedCurveManager manager(nh, tele, car_speed);
+		speed_curve_manager = &manager;
+		speed_curve_manager->initialize();
+	} else if (run_type == "roundabout") {
+        RoundaboutManager manager(nh, tele, car_speed);
+        roundabout_manager = &manager;
+        roundabout_manager->initialize();
     }
 
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 
 	ros::waitForShutdown();
-
-    if (traffic_manager) {
-        traffic_manager->stop_cars();
-        free(traffic_manager);
-    }
-
-    if (highway_manager) {
-        highway_manager->stop_cars();
-        free(highway_manager);
-    }
-
-    if (speed_curve_manager) {
-        speed_curve_manager->stop_cars();
-        free(speed_curve_manager);
-    }
 
 	return 0;
 }
