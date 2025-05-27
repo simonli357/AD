ba6b334ca8f6ed7b554c1c4992470f65379353d6
simonli357 <xi.yang.li@mail.mcgill.ca>
2025-05-27 14:08:05 -0400

    kb change

--- src/control/src/Controller.cpp
+++ src/control/src/Controller.cpp
@@ -1561,15 +1561,15 @@ void StateMachine::run() {
                 }
 
                 /* ---- Passive decay ---- */
-                if (ch != 'w' && ch != 's') {
-                    if (velocity > 0) {
-                        velocity -= COAST_DECAY;
-                        if (velocity < 0) velocity = 0;
-                    } else if (velocity < 0) {
-                        velocity += COAST_DECAY;
-                        if (velocity > 0) velocity = 0;
-                    }
-                }
+                // if (ch != 'w' && ch != 's') {
+                //     if (velocity > 0) {
+                //         velocity -= COAST_DECAY;
+                //         if (velocity < 0) velocity = 0;
+                //     } else if (velocity < 0) {
+                //         velocity += COAST_DECAY;
+                //         if (velocity > 0) velocity = 0;
+                //     }
+                // }
 
                 if (ch != 'a' && ch != 'd') {
                     if (steering_angle > 0) {
@@ -1589,7 +1589,7 @@ void StateMachine::run() {
                 printw("Press 'q' to quit.");
 
                 /* ---- Send command to vehicle ---- */
-                utils.publish_cmd_vel(steering_angle, velocity);
+                utils.publish_cmd_vel(-steering_angle, velocity);
             }
 
             /* ---- Shutdown ---- */
