cee093675ea14c778dd2c549f6cea2783426f9b1
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-28 18:12:11 -0400

    updates

--- src/control/src/Controller.cpp
+++ src/control/src/Controller.cpp
@@ -1683,7 +1683,7 @@ int main(int argc, char **argv) {
     std::thread callback_thread;
     std::unique_ptr<ros::AsyncSpinner> spinner;
     if (async) {
-        int num_threads = 4;
+        int num_threads = 1;
         spinner = std::make_unique<ros::AsyncSpinner>(num_threads);
         spinner->start();
         std::cout << "Async spinner started with " << num_threads << " threads" << std::endl;
