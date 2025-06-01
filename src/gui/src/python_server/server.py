34b0e43939a11b61a5c400d2882a8c6fbc586188
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-06-01 16:58:35 -0400

    fix-broadcast

--- src/gui/src/python_server/server.py
+++ src/gui/src/python_server/server.py
@@ -43,6 +43,7 @@ class Server:
                     time.sleep(0.5)
             print("Succesfully connected to host dashboard")
             self.tcp_client = TcpConnection(self.tcp_socket, self.on_packet, is_host=self.is_host, dashboard=True)
+            self.main_window.set_callbacks()
 
     def listen(self):
         while self.alive:
