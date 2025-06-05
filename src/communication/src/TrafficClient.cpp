6545f585c174254fa6893fe637ff79db23a88df1
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-06-05 18:30:44 -0400

    updates

--- src/communication/src/TrafficClient.cpp
+++ src/communication/src/TrafficClient.cpp
@@ -219,5 +219,5 @@ void TrafficClient::send_car_data(const Float32MultiArray &road_object) {
 
         std::string msg = v_pos + v_rot + v_speed + objcts;
         send(tcp_socket, msg.data(), msg.size(), 0);
-    };
+    });
 }
