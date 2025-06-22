d3fbc942ff558f731920858731043391b3df8892
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-06-22 03:00:24 -0400

    updates

--- src/gui/src/python_server/msg/imu_calib_msg.py
+++ src/gui/src/python_server/msg/imu_calib_msg.py
@@ -14,8 +14,8 @@ class ImuCalibMsg:
     def decode(self, bytes):
         splits = decoder.split(bytes)
         imu_calib_msg = ImuCalibMsg()
-        imu_calib_msg.sys_calib = struct.unpack('f', splits[0])[0]
-        imu_calib_msg.gyro_calib = struct.unpack('f', splits[1])[0]
-        imu_calib_msg.mag_calib = struct.unpack('f', splits[2])[0]
-        imu_calib_msg.accel_calib = struct.unpack('f', splits[3])[0]
+        imu_calib_msg.sys_calib = struct.unpack('<d', splits[0])[0]
+        imu_calib_msg.gyro_calib = struct.unpack('<d', splits[1])[0]
+        imu_calib_msg.mag_calib = struct.unpack('<d', splits[2])[0]
+        imu_calib_msg.accel_calib = struct.unpack('<d', splits[3])[0]
         return imu_calib_msg
