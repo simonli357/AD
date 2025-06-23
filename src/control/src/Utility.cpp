4e80e4a2635bd9f638fa2bd80928ad46ced4bcd9
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2025-06-23 01:07:46 -0400

    highway

--- src/control/src/Utility.cpp
+++ src/control/src/Utility.cpp
@@ -489,21 +489,28 @@ void Utility::process_lane_data(const utils::Lane3& msg) {
         if (Tunable::lane_relocalize2 && (ros::Time::now() - next_pose_reset_time).toSec() > 0) {
             // if (PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYLEFT)) {
             if (true) {
-                if((msg.good_left||msg.good_right) && std::max(0.0, PathManager::closest_waypoint_index - 1.5 * PathManager::density)+1 >= PathManager::overtake_end_index) {
+                if((msg.good_left||msg.good_right)) {
                     if(msg.lane_waypoints.size() > lane_waypoints.size()/3) {
                         double lane_wpt_y = msg.lane_waypoints[1];
                         bool proceed = true;
                         bool right_only = false;
-                        if (PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYLEFT)) {
-                            if (msg.good_right && !msg.right_waypoints.empty()) {
-                                lane_wpt_y = msg.right_waypoints[0] + 0.37/2.0; // 0.37 is the width of the lane
-                                right_only = true;
-                                // std::cout << "lane_wpt_y: " << lane_wpt_y << ", msg.right_waypoints[0]: " << msg.right_waypoints[0] << ", msg.left_waypoints[0]: " << msg.left_waypoints[0] << std::endl;
+                        if (PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYRIGHT) ||
+                            PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYLEFT)) {
+                            if ((PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYRIGHT) && std::max(0.0, PathManager::closest_waypoint_index - 1.5 * PathManager::density)+1 < PathManager::overtake_end_index) ||
+                                PathManager::attribute_cmp(PathManager::closest_waypoint_index, PathManager::ATTRIBUTE::HIGHWAYLEFT)) {
+                                if (msg.good_right && !msg.right_waypoints.empty()) {
+                                    // on highwayright, but in overtaking maneuver
+                                    lane_wpt_y = msg.right_waypoints[0] + 0.37/2.0; // 0.37 is the width of the lane
+                                    right_only = true;
+                                    // std::cout << "lane_wpt_y: " << lane_wpt_y << ", msg.right_waypoints[0]: " << msg.right_waypoints[0] << ", msg.left_waypoints[0]: " << msg.left_waypoints[0] << std::endl;
+                                }
                             } else {
                                 proceed = false;
                                 // std::cout << "process_lane_data(): HIGHWAYLEFT but no good_right, skipping lane relocation" << std::endl;
                             }
                             // proceed = false;
+                        } else if (std::max(0.0, PathManager::closest_waypoint_index - 1.5 * PathManager::density)+1 < PathManager::overtake_end_index) {
+                            proceed = false;
                         }
                         double near_m = msg.near_m;
                         double lane_wpt_x = msg.lane_waypoints[0] + near_m;
