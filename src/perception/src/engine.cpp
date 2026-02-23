98e4a2d6747c743b8ca81ec6f8a5b3f4e91454a9
simonli357 <xi.yang.li@mail.mcgill.ca>
2026-02-23 16:46:25 -0500

    trt10 compaitbility

--- src/perception/src/engine.cpp
+++ src/perception/src/engine.cpp
@@ -403,7 +403,12 @@ bool Engine::runInference(const std::vector<std::vector<cv::cuda::GpuMat>> &inpu
     for (int batch = 0; batch < batchSize; ++batch) {
         // Batch
         std::vector<std::vector<float>> batchOutputs{};
-        for (int32_t outputBinding = numInputs; outputBinding < m_engine->getNbBindings(); ++outputBinding) {
+#if NV_TENSORRT_MAJOR < 9
+        int nb = m_engine->getNbBindings();
+#else
+        int nb = m_engine->getNbIOTensors();
+#endif
+        for (int32_t outputBinding = numInputs; outputBinding < nb; ++outputBinding) {
             // We start at index m_inputDims.size() to account for the inputs in our m_buffers
             std::vector<float> output;
             auto outputLenFloat = m_outputLengthsFloat[outputBinding - numInputs];
